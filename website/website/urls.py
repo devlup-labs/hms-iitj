f107795e58769b1614f1ad53ccb7723c6b416f6b
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2020-02-24 23:41:09 +0530

    created urls.py for accounts

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    
+
     # path('hc', include('hc.urls')),
     # path('users', include('users.urls')),
 ]
