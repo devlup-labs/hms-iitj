ff17b114934b389a9736b55127bbde947492683b
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-02-25 00:31:51 +0530

    added login

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -1,6 +1,7 @@
-# from django.conf.urls import url
-# from accounts import views
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
-# urlpatterns={
-
-# }
+urlpatterns = [
+    url(r'^login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    url(r'^logout', auth_views.LogoutView.as_view(), name='logout'),
+]
