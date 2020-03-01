80564a6d7abf701af96ef033eb1ac800d82ff6ea
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-03-01 13:37:23 +0530

    changed UI

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+app_name = 'accounts'
+
 urlpatterns = [
     url(r'^login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     url(r'^logout', auth_views.LogoutView.as_view(), name='logout'),
