74451759ca325c13395d2346c7f69922265f7243
Harsh Anand <anand.2@iitj.ac.in>
2021-04-21 03:10:08 +0530

    remove appointments by doctor from urls

--- main/urls.py
+++ main/urls.py
@@ -12,12 +12,10 @@ urlpatterns = [
     path('', IndexView, name='home'),
     path('doctor/', IndexViewDoctor, name='home_doctor'),
     path('receptionist/', IndexViewReceptionist, name='home_receptionist'),
-    path('receptionist/appointments/<str:doc_name>', AppointmentsOfDoctor, name='recep_appointments'),
     path('pharmacist/', IndexViewPharmacist, name='home_pharmacist'),
     url(r'^blogs/(?P<slug>[\w-]+)/$', BlogDetailsView.as_view(), name='blog_details'),
     url(r'^blogs/edit/(?P<slug>[\w-]+)/$', EditBlogView, name='edit_blog'),
     path('addblog/', AddBlogView.as_view(), name='AddBlog'),
     path('developers/', DevelopersPage, name='developers'),
     path('hitcount/', include(('hitcount.urls', 'hitcount'), namespace='hitcount')),
-
 ]
